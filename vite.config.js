ea6217e53aba91cd88720b5d90b323846181c24f
Abhay prajapati <abhayparja90@gmail.com>
2024-11-22 14:13:39 +0530

    jkak

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/<repo-name>/' // Replace <repo-name> with your GitHub repo name
 });
