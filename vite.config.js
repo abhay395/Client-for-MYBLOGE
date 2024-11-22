674f19e48793ed50d33eba4ea26928675f968525
Abhay prajapati <abhayparja90@gmail.com>
2024-11-22 14:35:07 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repo-name>/' // Replace <repo-name> with your GitHub repo name
-});
+  base: '/Client-for-MYBLOGE/' // Replace <your-repository-name> with your GitHub repository name
+})
