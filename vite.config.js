7a70263f3b4c78adeb8c6f80cc62add59b95ecca
Abhay prajapati <abhayparja90@gmail.com>
2024-11-22 13:59:22 +0530

    commite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Example chunk splitting
-        },
-      },
-    },
-  },
-})
+  base: '/'
+});
